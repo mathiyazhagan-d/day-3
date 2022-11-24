8c61e8de32391f441d6dbc76a08f407b4a5cee0c
mathiyazhagan-d <116990455+mathiyazhagan-d@users.noreply.github.com>
2022-11-24 22:36:30 +0530

    Add files via upload

---
+++ for.js
@@ -0,0 +1,68 @@
+// question 1 for ,for in, for of and for each
+let marketItems={
+    morning:["bread","milk","egg"],
+    lunch:["bread","milk","egg"],
+    night:["bread","milk","egg","rice"]
+}
+/* output for 
+bread
+milk
+egg
+rice
+*/
+
+// for loop to print food for night
+
+for(let i=0;i<marketItems.night.length;i++){
+    console.log(marketItems.night[i]);
+}
+
+// for in
+
+
+for(let key in marketItems){
+   
+    console.log(key,marketItems[key]);
+    
+}
+
+/* output for in
+morning [ 'bread', 'milk', 'egg' ]
+lunch [ 'bread', 'milk', 'egg' ]
+night [ 'bread', 'milk', 'egg', 'rice' ]
+
+*/
+
+let foodItems= ["bread","milk","egg","rice"];
+    
+// for of
+
+for(let index of foodItems){
+    console.log(index);
+}
+ /*
+ output for of
+ bread
+milk
+egg
+rice
+
+*/
+
+
+// for each
+
+let a=[1,2,3,4];
+
+a.forEach((num)=>console.log(num))
+
+/*
+output foreach
+1
+2
+3
+4
+*/
+//----------------------------------------------------------------------------------------------------------------------------------
+
+
