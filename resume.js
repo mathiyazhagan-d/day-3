3bb8dbfdb32b7878f34eacbd8b41c16f87be32f6
mathiyazhagan-d <116990455+mathiyazhagan-d@users.noreply.github.com>
2022-11-26 20:17:49 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,33 @@
+let resume=[{
+    "about": 
+        {
+            "name": "Mathiyazhagan D",
+            "email": "mathi@gmail.com",
+            "mobile": "997766453",
+            "date of birth": "3.05.1997",
+            "age": "25",
+            "native": "chennai",
+            "pincode": "600001",
+            "language known": "Tamil and English"
+        }
+    ,
+    "education": 
+        {
+            "senior secondary percentile": "90%",
+            "higher secondary percentile": "80%",
+            "degree": "Bachelor of engineering",
+            "degree major": "Electrical and Electronics Engineering",
+            "degree percentile": "6.40%"
+        }
+    ,
+     "technical skills": 
+        {
+            "Programming language": "Basics of java ,c and c++",
+            "Softwares known": "Adobe Photoshop, After effects",
+        }
+    
+    
+        
+    
+}];
+  console.log(resume);
